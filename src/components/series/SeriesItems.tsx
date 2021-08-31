5d2893519c56df6c6f690cb70d915e92a0c53b17
Lucas-knut <bwb.steichen.l@fondespierre.com>
2021-08-31 08:02:10 +0000

    pages delete

--- src/components/series/SeriesItems.tsx
+++ src/components/series/SeriesItems.tsx
@@ -22,19 +22,19 @@ import {
     function displayIconColor() {
       if (serie.total == null) {
         console.log("Non fait");
-        return <IonIcon slot="end" className="ellipseoutline" icon={ellipseOutline} />
+        return <IonIcon slot="end" size="large" className="ellipseoutline" icon={ellipseOutline} />
 
       } else if (serie.total>=81 && serie.total<=100) {
         console.log("90%");
-        return <IonIcon slot="end" className="checkmarkcircle" icon={checkmarkCircle} />
+        return <IonIcon slot="end" size="large" className="checkmarkcircle" icon={checkmarkCircle} />
 
       } else if (serie.total>=51 && serie.total<=80) {
         console.log("70%");
-        return <IonIcon slot="end" className="removecircle" icon={removeCircle} />
+        return <IonIcon slot="end" size="large" className="removecircle" icon={removeCircle} />
 
       } else if (serie.total<=50) {
         console.log("40%");
-        return <IonIcon slot="end" className="closecircle" icon={closeCircle} />
+        return <IonIcon slot="end" size="large" className="closecircle" icon={closeCircle} />
 
       }
     }
