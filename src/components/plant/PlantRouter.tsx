9bbedc42ada327f2123c540a2a880cce74c4dc57
gitSushi <lossushi37@gmail.com>
2021-08-31 14:35:42 +0000

    working test ionSegment

--- src/components/plant/PlantRouter.tsx
+++ src/components/plant/PlantRouter.tsx
@@ -1,8 +1,11 @@
 import { Route } from 'react-router-dom';
 import { IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonSegment, IonSegmentButton, IonLabel } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { useState } from 'react';
 
 const PlantRouter: React.FC = () => {
+
+    const [state, setstate] = useState("personnel")
 
     return (
         <div>
@@ -12,7 +15,11 @@ const PlantRouter: React.FC = () => {
                     <IonTitle>Segment</IonTitle>
                 </IonToolbar>
                 <IonToolbar>
-                    <IonSegment onIonChange={e => console.log('Segment selected', e.detail.value)} value="personel">
+                    <IonSegment
+                        onIonChange={e => {
+                            if (e.detail.value !== undefined) setstate(e.detail.value);
+                        }}
+                        value={state}>
                         <IonSegmentButton value="personnel">
                             <IonLabel>Personnel</IonLabel>
                         </IonSegmentButton>
@@ -23,14 +30,21 @@ const PlantRouter: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
 
-            <IonRouterOutlet>
-                <Route exact path="/plants/public">
-                    <p>PUBLIC ZZZ</p>
-                </Route>
-                <Route exact path="/plants/perso">
-                    <p>PERSONAL YYY</p>
-                </Route>
-            </IonRouterOutlet>
+            {
+                state === "personnel" ? (
+                    <p>perso</p>
+                ) : (
+                    <p>pub</p>
+                )
+            }
+            {/* <Route exact path="/plants/public">
+                <p>PUBLIC ZZZ</p>
+            </Route>
+            <Route exact path="/plants/perso">
+                <p>PERSONAL YYY</p>
+            </Route> */}
+            {/* <IonRouterOutlet>
+            </IonRouterOutlet> */}
         </div>
     );
 };
