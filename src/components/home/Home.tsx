f5c2c246c50976a56d7acdad7f3337b624b34430
Lucas-knut <bwb.steichen.l@fondespierre.com>
2021-09-02 14:18:17 +0000

    in progress

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,4 +1,6 @@
-import { IonButton, IonContent, IonPage } from "@ionic/react"
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonPage, IonRefresher, IonRefresherContent, IonTitle, IonToolbar } from "@ionic/react"
+import { pin, wifi, wine, warning, walk } from "ionicons/icons";
+import SeriesItems from "../series/SeriesItems";
 
 
 /**
@@ -6,13 +8,71 @@ import { IonButton, IonContent, IonPage } from "@ionic/react"
  */
 const Home: React.FC = () => {
     console.log("home")
+
+
+    const refresh = (e: CustomEvent) => {
+        setTimeout(() => {
+            e.detail.complete();
+        }, 3000);
+    };
+
     return (
+
         <IonPage>
-            <IonContent>
-                <IonButton href="/">back</IonButton>
-                <h2>Home</h2>
-            </IonContent>
-        </IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>CardExamples</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+              <IonCardTitle>Card Title</IonCardTitle>
+            </IonCardHeader>
+  
+            <IonCardContent>
+              Keep close to Nature's heart... and break clear away, once in awhile,
+              and climb a mountain or spend a week in the woods. Wash your spirit clean.
+        </IonCardContent>
+          </IonCard>
+  
+          <IonCard>
+            <IonItem>
+              <IonIcon icon={pin} slot="start" />
+              <IonLabel>ion-item in a card, icon left, button right</IonLabel>
+              <IonButton fill="outline" slot="end">View</IonButton>
+            </IonItem>
+  
+            <IonCardContent>
+              This is content, without any paragraph or header tags,
+              within an ion-cardContent element.
+        </IonCardContent>
+          </IonCard>
+  
+          <IonCard>
+            <IonItem href="#" className="ion-activated">
+              <IonIcon icon={wifi} slot="start" />
+              <IonLabel>Card Link Item 1 activated</IonLabel>
+            </IonItem>
+  
+            <IonItem href="#">
+              <IonIcon icon={wine} slot="start" />
+              <IonLabel>Card Link Item 2</IonLabel>
+            </IonItem>
+  
+            <IonItem className="ion-activated">
+              <IonIcon icon={warning} slot="start" />
+              <IonLabel>Card Button Item 1 activated</IonLabel>
+            </IonItem>
+  
+            <IonItem>
+              <IonIcon icon={walk} slot="start" />
+              <IonLabel>Card Button Item 2</IonLabel>
+            </IonItem>
+          </IonCard>
+        </IonContent>
+      </IonPage>
     );
 };
 
