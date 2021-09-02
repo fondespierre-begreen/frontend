6a202d3cc0516f7d336c018d32d9bf4c81fadc2f
Lucas-knut <bwb.steichen.l@fondespierre.com>
2021-09-02 15:41:12 +0000

    Dont work

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -4,7 +4,8 @@ import {
     IonTitle,
     IonContent,
     IonImg,
-    IonButton
+    IonButton,
+    IonToolbar
 } from "@ionic/react";
 
 import "./login.css";
@@ -17,13 +18,16 @@ const Login: React.FC = () => {
     return (
         <IonPage>
             <IonHeader>
-                <IonTitle>Login nya nya</IonTitle>
+                <IonToolbar>
+                    <IonTitle>Page de connexion</IonTitle>
+                </IonToolbar>
             </IonHeader>
+            <div className="background-container">
             <IonContent fullscreen>
-                <IonImg className="login-img" src="https://github.com/fondespierre-begreen/documentation/blob/main/photos/AdobeStock_202538386-1024x683.jpeg?raw=true"></IonImg>
                 <IonButton href="/visitor" color="success" expand="block">Visiteurs</IonButton>
                 <IonButton href="/connected" fill="clear" color="light" expand="block">Connexion</IonButton>
             </IonContent>
+            </div>
         </IonPage>
     )
 }
