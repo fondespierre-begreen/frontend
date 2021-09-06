0505c8c2cd7e9708bca2a8bcf1b7948115d6d830
Alexandre Labsi <alex.lbs2001@gmail.com>
2021-09-06 12:33:31 +0000

    fix params display

--- src/components/parameter/Parameter.tsx
+++ src/components/parameter/Parameter.tsx
@@ -20,45 +20,47 @@ const Parameter: React.FC = () => {
         <IonTitle color="success" slot="secondary">Paramètres</IonTitle>
       </IonToolbar>
 
-      <IonContent fullscreen>
-        <IonList>
-          <IonItem routerLink="/settings/account" detail={false}>
-            <IonIcon slot="start" icon={personCircleOutline} size="large" />
-            <IonLabel className="ion-text-wrap">
-              <h2>
-                Account
-              </h2>
-            </IonLabel>
-            <IonIcon slot="end" icon={chevronForward} size="default" />
-          </IonItem>
+      <IonContent fullscreen>  
+        <div className="dorine">
+          <div>
+            <IonList>
+                <IonItem routerLink="/settings/account" detail={false}>
+                  <IonIcon slot="start" icon={personCircleOutline} size="large" />
+                  <IonLabel className="ion-text-wrap">
+                    <h2>
+                      Account
+                    </h2>
+                  </IonLabel>
+                  <IonIcon slot="end" icon={chevronForward} size="default" />
+                </IonItem>
 
-          <IonItem routerLink="/settings/help" detail={false}>
-            <IonIcon slot="start" icon={helpCircleOutline} size="large" />
-            <IonLabel className="ion-text-wrap">
-              <h2>
-                Aide
-              </h2>
-            </IonLabel>
-            <IonIcon slot="end" icon={chevronForward} size="default" />
-          </IonItem>
+                <IonItem routerLink="/settings/help" detail={false}>
+                  <IonIcon slot="start" icon={helpCircleOutline} size="large" />
+                  <IonLabel className="ion-text-wrap">
+                    <h2>
+                      Aide
+                    </h2>
+                  </IonLabel>
+                  <IonIcon slot="end" icon={chevronForward} size="default" />
+                </IonItem>
 
-          <IonItem routerLink="/settings/about" detail={false}>
-            <IonIcon slot="start" icon={informationCircleOutline} size="large" />
-            <IonLabel className="ion-text-wrap">
-              <h2>
-                À propos
-              </h2>
-            </IonLabel>
-            <IonIcon slot="end" icon={chevronForward} size="default" />
-          </IonItem>
-        </IonList>
-
-        <IonImg className="parametres-img" src="https://github.com/fondespierre-begreen/documentation/blob/main/logos/beGreenSigle.png?raw=true"></IonImg>
-
-        <IonButton href="https://fondespierre.com/nos-poles-de-competences/begreen/" className="site-begreen" fill="clear" expand="block">Site web Fondespierre BeGreen</IonButton>
-
-        <IonButton href="#" fill="clear" expand="block" color="danger">Déconnexion</IonButton>
-
+                <IonItem routerLink="/settings/about" detail={false}>
+                  <IonIcon slot="start" icon={informationCircleOutline} size="large" />
+                  <IonLabel className="ion-text-wrap">
+                    <h2>
+                      À propos
+                    </h2>
+                  </IonLabel>
+                  <IonIcon slot="end" icon={chevronForward} size="default" />
+                </IonItem>
+              </IonList>
+          </div>
+          <div>
+            <IonImg className="parametres-img" src="https://github.com/fondespierre-begreen/documentation/blob/main/logos/beGreenSigle.png?raw=true"></IonImg>
+            <IonButton href="https://fondespierre.com/nos-poles-de-competences/begreen/" className="site-begreen" fill="clear" expand="block">Site web Fondespierre BeGreen</IonButton>
+            <IonButton href="#" fill="clear" expand="block" color="danger">Déconnexion</IonButton>
+          </div>
+        </div>
         </IonContent>
     </IonPage>
   );
