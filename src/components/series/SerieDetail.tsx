57fb99ddbba03f462875a8e96efe0042bb57da67
gitSushi <lossushi37@gmail.com>
2021-09-18 09:43:24 +0000

    cleaned and checked only the post left to do

--- src/components/series/SerieDetail.tsx
+++ src/components/series/SerieDetail.tsx
@@ -16,60 +16,37 @@ import {
   IonList,
   IonRadio,
   IonRadioGroup,
-  IonIcon,
-  withIonLifeCycle,
-  useIonViewWillEnter,
-  useIonViewWillLeave,
-  useIonViewDidEnter,
-  useIonViewDidLeave
+  IonIcon
 } from '@ionic/react';
 import { chevronBack } from 'ionicons/icons';
+
 import React, { useState, useEffect } from 'react';
 import { RouteComponentProps, useLocation, useRouteMatch } from 'react-router-dom';
 
-import { getCheckedChoices, getquestions, getTest, postSerie } from './seriesService';
+import { getCheckedChoices, getquestions, getTest, IQuizParams, postSerie } from './seriesService';
+
 import "./seriedetail.css"
 
 
-interface IQuizParams {
-  qId: string;
-  tId: string;
-}
-
+/**
+ * @returns La vue pour que l'apprenant passe un test
+ */
 const SerieDetail: React.FC<RouteComponentProps> = () => {
-  let { params } = useRouteMatch();
+  const { params } = useRouteMatch();
 
-  let seriesId: IQuizParams = params as IQuizParams;
-  let questId: IQuizParams = params as IQuizParams;
-
-
+  const seriesId: IQuizParams = params as IQuizParams;
+  const questId: IQuizParams = params as IQuizParams;
   const tId = parseInt(seriesId.tId); // 1
   const qId = parseInt(questId.qId); // 0
 
   const [quiz] = useState<any>(getquestions(tId));
-
   const [selected, setSelected] = useState<string>();
 
-  // let choices = getquestions(tId)[qId].choices
-
-  // let serieQuestions = getquestions(tId)
-
-
-  const location = useLocation()
-
-
-  // useIonViewWillEnter(() => {
-  //   const gChoices = getCheckedChoices()
-  //   setSelected(gChoices[qId - 1]);
-  // });
+  const location = useLocation();
 
   useEffect(() => {
-    // const gChoices = getCheckedChoices()
-
-    // console.log(qId);
-    setSelected("");
+    setSelected("force la variable select a changer // ceci est un commentaire");
   }, [location]);
-
 
   /**
    * Enregistre le choice checked
@@ -79,126 +56,98 @@ const SerieDetail: React.FC<RouteComponentProps> = () => {
 
     if (choiceValue?.value !== undefined) {
 
-      const arrayEmpty = localStorage.getItem('checkedChoices')
-      const checkChoices = JSON.parse(arrayEmpty!)
+      const arrayEmpty = localStorage.getItem('checkedChoices');
+      const checkChoices = JSON.parse(arrayEmpty!);
 
       // Ajoute ou écrase la check value au même index que la question dans un tableau
-      checkChoices[qId] = choiceValue.value
+      checkChoices[qId] = choiceValue.value;
 
-      localStorage.setItem('checkedChoices', JSON.stringify(checkChoices))
-      // choices = [...choices, choiceValue.value]
+      localStorage.setItem('checkedChoices', JSON.stringify(checkChoices));
+      // choices = [...choices, choiceValue.value];
       console.log(checkChoices);
-
-      // resetRadio(choiceValue)
     }
   }
 
   /**
-   * Reset le radio checked
+   * select (et donc le radio button) prend la valeur enregistrée (la réponse de l'apprenant)
    */
-  function resetRadio(choiceValue: any) {
-    // choiceValue.value = ""
-  }
-
-  /**
-   * Enregistre le choice checked courant au bouton suivant
-   */
-  function registerNext() {
-    registerValue()
-  };
-
-  /**
-   * Enregistre le choice checked courant au bouton précédent
-   */
-  function registerBack() {
-    registerValue()
-  }
-
-  const handleOnChange = (event: any) => {
-    const gChoices = getCheckedChoices()
+  const handleOnChange = () => {
+    const gChoices = getCheckedChoices();
     setSelected(gChoices[qId]);
   };
-
-
 
   /**
    * Supprime les id des propriétés serie, questions et choices afin de push le tableau dans le back qui va rajouter les id incrémentés
    * @param serie tableau serie
    */
   function cleanSerie(serie: any) {
-    delete serie.id
-    serie.questions.map((q: any) => delete q.id)
-    serie.questions.map((q: any) => q.choices.map((c: any) => delete c.id))
+    delete serie.id;
+    serie.questions.map((q: any) => delete q.id);
+    serie.questions.map((q: any) => q.choices.map((c: any) => delete c.id));
   }
 
   const sendTest = () => {
-
     // Enregistre le choice checked de la dernière question
-    registerValue()
+    registerValue();
 
-    const serie = getTest(tId)
+    const serie = getTest(tId);
 
 
     // Liste des choix checked
-    let arrChoicesChecked: any = JSON.parse(localStorage.getItem('checkedChoices')!)
+    let arrChoicesChecked: any = JSON.parse(localStorage.getItem('checkedChoices')!);
     console.log(arrChoicesChecked);
 
     // Ajout de l'id du choix correspondant à la réponse de l'user
     serie.questions.map((question: any, i: number) => {
       question.choices.map((choice: any) => {
         if (choice.description === arrChoicesChecked[i]) {
-          question.answers[0] = { id: choice.id }
+          question.answers[0] = { id: choice.id };
           console.log(choice.id);
 
-          // localStorage.setItem('result', JSON.stringify(serie))
+          // localStorage.setItem('result', JSON.stringify(serie));
         }
       })
     })
 
     // Supprime les id du tableau
-    cleanSerie(serie)
+    cleanSerie(serie);
     console.log(serie);
 
 
     // console.log(JSON.stringify(serie));
-    let currentSerie = {}
+    let currentSerie = {};
     postSerie(serie)
-      .then(serie => currentSerie = serie)
-
+      .then(serie => currentSerie = serie);
   }
 
-
-
   /**
-   * 
    * @param qIdCurrent Id courant de la question
    * @param questions Liste des questions
    * @returns Les boutons nécessaires à la navigation entre les questions
    */
   function displayButtons(qIdCurrent: number, questions: any) {
-
     if (qIdCurrent == 0) {
       return (
         <div className="button-next">
-          <IonButton onClick={registerNext} color="success" routerLink={`/connected/series/${tId}/quest/${qId + 1}`}>Suivant</IonButton>
+          <IonButton onClick={registerValue} color="success" routerLink={`/connected/series/${tId}/quest/${qId + 1}`}>Suivant</IonButton>
         </div>
-      )
+      );
 
     } else if (qIdCurrent == questions.length - 1) {
       return (
         <div className="buttons">
-          <IonButton onClick={registerBack} color="success" routerDirection="back" routerLink={`/connected/series/${tId}/quest/${qId - 1}`}>Précédent</IonButton>
+          <IonButton onClick={registerValue} color="success" routerDirection="back" routerLink={`/connected/series/${tId}/quest/${qId - 1}`}>Précédent</IonButton>
           <IonButton onClick={sendTest} color="danger" routerLink={`/connected/series/${tId}/quest/${qId}`}>Envoyer</IonButton>
         </div>
-      )
+      );
 
     } else {
       return (
         <div className="buttons">
-          <IonButton onClick={registerBack} color="success" routerDirection="back" routerLink={`/connected/series/${tId}/quest/${qId - 1}`}>Précédent</IonButton>
-          <IonButton onClick={registerNext} color="success" routerLink={`/connected/series/${tId}/quest/${qId + 1}`}>Suivant</IonButton>
+          <IonButton onClick={registerValue} color="success" routerDirection="back" routerLink={`/connected/series/${tId}/quest/${qId - 1}`}>Précédent</IonButton>
+          <IonButton onClick={registerValue} color="success" routerLink={`/connected/series/${tId}/quest/${qId + 1}`}>Suivant</IonButton>
         </div>
-      )
+      );
     }
   }
 
@@ -252,7 +201,3 @@ const SerieDetail: React.FC<RouteComponentProps> = () => {
 };
 
 export default SerieDetail;
-function ionViewWillEnter() {
-  throw new Error('Function not implemented.');
-}
-
--- src/components/series/seriesService.ts
+++ src/components/series/seriesService.ts
@@ -1,29 +1,77 @@
-// const series = fetch(`http://192.168.1.46:9090/series`)
-//                     .then(res => res.json())
-
-
-localStorage.setItem('checkedChoices', JSON.stringify([]))
-
-export const getCheckedChoices = () => {
-  const lSCCs = localStorage.getItem('checkedChoices');
-  const parsedLSCCs = JSON.parse(lSCCs!)
-  return parsedLSCCs;
-}
-
-fetch(`http://localhost:9090/series`)
-  .then(res => res.json())
-  .then(data => {
-    localStorage.setItem('test', JSON.stringify(data))
-  })
-
-
-
+/**
+ * #############################################################################
+ * interface
+ */
 export interface ISerie {
   id: number;
   total: number | null;
   createdat: string;
 }
 
+export interface IQuizParams {
+  qId: string;
+  tId: string;
+}
+
+/**
+ * #############################################################################
+ * localStorage
+ */
+localStorage.setItem('checkedChoices', JSON.stringify([]));
+
+export const getCheckedChoices = () => {
+  const lSCCs = localStorage.getItem('checkedChoices');
+  const parsedLSCCs = JSON.parse(lSCCs!);
+  return parsedLSCCs;
+}
+
+/**
+ * @param tId 
+ * @returns tableau d'objets question
+ */
+export const getquestions = (tId: number) => {
+  const serie = JSON.parse(localStorage.getItem('test')!);
+  const test = serie!.filter((s: any) => s.id == tId);
+
+  // index à 0 serie id est 1
+  return test[0].questions;
+}
+
+export const getTest = (tId: number) => {
+  const serie = JSON.parse(localStorage.getItem('test')!);
+  const test = serie!.filter((s: any) => s.id == tId);
+
+  // index à 0 serie id est 1
+  return test[0];
+}
+
+/**
+ * #############################################################################
+ * fetch
+ */
+
+fetch(`http://localhost:9090/series`)
+  .then(res => res.json())
+  .then(data => {
+    localStorage.setItem('test', JSON.stringify(data))
+  });
+
+export const postSerie = (serie: any) => {
+  return fetch(`http://localhost:9090/series`, {
+    method: "POST",
+    headers: {
+      'Accept': '*/*',
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(serie),
+  }).then(resp => resp.json());
+}
+
+
+/**
+ * #############################################################################
+ * mock data
+ */
 const series: ISerie[] = [
   {
     id: 1,
@@ -45,55 +93,6 @@ const series: ISerie[] = [
     total: 40,
     createdat: "12 janvier 2021"
   }
-]
-
-
+];
 
 export const getSeries = () => series;
-
-/**
- * 
- * @param tId 
- * @returns tableau d'objets question
- */
-export const getquestions = (tId: number) => {
-
-  const serie = JSON.parse(localStorage.getItem('test')!)
-
-  // console.log(serie![tId].questions, "serv");
-
-  const test = serie!.filter((s: any) => s.id == tId)
-
-
-  // index à 0 serie id est 1
-  return test[0].questions
-
-}
-
-export const getTest = (tId: number) => {
-
-  const serie = JSON.parse(localStorage.getItem('test')!)
-
-  // console.log(serie![tId].questions, "serv");
-
-  const test = serie!.filter((s: any) => s.id == tId)
-
-
-  // index à 0 serie id est 1
-  return test[0]
-
-}
-
-
-
-
-export const postSerie = (serie: any) => {
-  return fetch(`http://localhost:9090/series`, {
-    method: "POST",
-    headers: {
-      'Accept': '*/*',
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify(serie),
-  }).then(resp => resp.json())
-}
