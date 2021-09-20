b3836f5f2cb456fe7118b7d1bca76c8e56e7f846
Lucas-knut <bwb.steichen.l@fondespierre.com>
2021-09-20 09:44:01 +0000

    Change logo BeGreen png to svg on login page and parameters page so as not to have any loss of quality

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -16,7 +16,7 @@ const Login: React.FC = () => {
         <IonPage>
             <IonContent fullscreen className="background">
                 <div className="flex">
-                    <img src="http://image.noelshack.com/fichiers/2021/36/1/1630920868-begreenblancweb.png" alt="begreen" />
+                    <img src="https://raw.githubusercontent.com/fondespierre-begreen/documentation/aa2a5eab5ff070b22b98aaa913b675073b4f26c4/logos/beGreenBlancWeb.svg" alt="begreen" />
                     <div className="btn">
                         <IonButton href="/visitor" fill="solid" color="success" expand="block">Visiteurs</IonButton>
                         <IonButton href="/connected"  fill="outline" color="light" expand="block">Connexion</IonButton>
